725277e8861ac33792b6393e27d2d7f9182c639c
Daine Bigham <daine.bigham@proton.me>
2024-11-19 11:39:30 +1300

    Moved caeser class to separate file - encryption.py

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-class CaesarCipher:
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+import src.encryption
 
-    def __init__(self, shift):
-        self.shift = shift % 26
-
-    def encrypt(self, message):
-        return self._cipher(message, self.shift)
-    
-    def decrypt(self, message):
-        return self._cipher(message, -self.shift)
-
-    def _cipher(self, message, shift):
-        result = []
-        for char in message.lower():
-            if char.isalpha():
-                index = (self.alphabet.index(char) + shift) % 26
-                result.append(self.alphabet[index])
-            else:
-                result.append(char)
-        return ''.join(result)
-    
 def main():
     shift = int(input("Enter the shift value: "))
-    caeser = CaesarCipher(shift)
+    caesar = src.encryption.CaesarCipher(shift)
 
     message = input("Enter message to encrypt: ")
-    encrypted = caeser.encrypt(message)
+    encrypted = caesar.encrypt(message)
     print(f"Encrypted Message: {encrypted}")
+
+    decrypted = caesar.decrypt(encrypted)
+    print(f"Decrypted message: {decrypted}")
 
 if __name__ == "__main__":
     main()
---
+++ src/encryption.py
@@ -0,0 +1,21 @@
+class CaesarCipher:
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    def __init__(self, shift):
+        self.shift = shift % 26
+
+    def encrypt(self, message):
+        return self._cipher(message, self.shift)
+    
+    def decrypt(self, message):
+        return self._cipher(message, -self.shift)
+
+    def _cipher(self, message, shift):
+        result = []
+        for char in message.lower():
+            if char.isalpha():
+                index = (self.alphabet.index(char) + shift) % 26
+                result.append(self.alphabet[index])
+            else:
+                result.append(char)
+        return ''.join(result)
